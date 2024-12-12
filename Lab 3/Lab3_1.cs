9e5d2dffa2f9c02a404e67421ff95e37e42435a7
Brind Sanandiya <brindbsan7@gmail.com>
2024-12-12 10:25:52 +0530

    Add files via upload

---
+++ Lab 3/Lab3_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Lab3_1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter first number: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter second number: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(a / b);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ Lab 3/Lab3_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Lab3_2
+    {
+        static void Main(string[] args) 
+        {
+            try 
+            {
+                int[] arr = new int[5];
+                while (true) 
+                {
+                    arr[i] = new int();
+                    int++;
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+    }
+}
